f00dbed9980f89bd02cecf06f1a2c37e49db7f62
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-05-08 12:48:00 +0700

    Penambahan AddProduct.jsx

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+
+function App() {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<ProductList/>}/>
+                <Route path="add" element={<AddProduct/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
