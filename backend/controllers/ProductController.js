eb1945d4c186b1b957afbb3191c1b8e883cb4f1a
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-05-10 12:08:58 +0700

    Pembaruan Kode

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -2,7 +2,7 @@ import Product from "../models/ProductModel.js";
 import path from "path";
 import fs from "fs";
 
-export const getProducts = async (req, res) =>{
+export const getProducts = async(req, res)=>{
     try {
         const response = await Product.findAll();
         res.json(response);
@@ -11,10 +11,10 @@ export const getProducts = async (req, res) =>{
     }
 }
 
-export const getProductById = async (req, res) =>{
+export const getProductById = async(req, res)=>{
     try {
         const response = await Product.findOne({
-            where: {
+            where:{
                 id : req.params.id
             }
         });
@@ -24,7 +24,7 @@ export const getProductById = async (req, res) =>{
     }
 }
 
-export const saveProduct = (req, res) =>{
+export const saveProduct = (req, res)=>{
     if(req.files === null) return res.status(400).json({msg: "No File Uploaded"});
     const name = req.body.title;
     const file = req.files.file;
@@ -32,85 +32,83 @@ export const saveProduct = (req, res) =>{
     const ext = path.extname(file.name);
     const fileName = file.md5 + ext;
     const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
-    const allowedType = ['.png', '.jpg', 'jpeg'];
+    const allowedType = ['.png','.jpg','.jpeg'];
 
-    if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg: "Invalid Images"})
-
+    if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg: "Invalid Images"});
     if(fileSize > 5000000) return res.status(422).json({msg: "Image must be less than 5 MB"});
 
-    file.mv(`./public/images/${fileName}`, async(err) => {
-        if(err) return res.status(500).json({msg: err.msg});
+    file.mv(`./public/images/${fileName}`, async(err)=>{
+        if(err) return res.status(500).json({msg: err.message});
         try {
             await Product.create({name: name, image: fileName, url: url});
-            res.status(201).json({msg: "Product Created Successfully"})
+            res.status(201).json({msg: "Product Created Successfuly"});
         } catch (error) {
             console.log(error.message);
         }
-    });
+    })
+
 }
 
-export const updateProduct = async (req, res) =>{
+export const updateProduct = async(req, res)=>{
     const product = await Product.findOne({
-        where: {
+        where:{
             id : req.params.id
         }
     });
     if(!product) return res.status(404).json({msg: "No Data Found"});
+
     let fileName = "";
     if(req.files === null){
-        fileName = Product.image;
+        fileName = product.image;
     }else{
         const file = req.files.file;
         const fileSize = file.data.length;
         const ext = path.extname(file.name);
         fileName = file.md5 + ext;
-        const allowedType = ['.png', '.jpg', 'jpeg'];
+        const allowedType = ['.png','.jpg','.jpeg'];
 
-        if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg: "Invalid Images"})
-
+        if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg: "Invalid Images"});
         if(fileSize > 5000000) return res.status(422).json({msg: "Image must be less than 5 MB"});
 
-        // berfungsi untuk menghapus image yang lama
-        const filePath = `./public/images/${product.image}`;
-        fs.unlinkSync(filePath);
+        const filepath = `./public/images/${product.image}`;
+        fs.unlinkSync(filepath);
 
-        file.mv(`./public/images/${fileName}`, (err) => {
-            if(err) return res.status(500).json({msg: err.msg});
+        file.mv(`./public/images/${fileName}`, (err)=>{
+            if(err) return res.status(500).json({msg: err.message});
         });
     }
     const name = req.body.title;
     const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
 
-    // untuk menyimpan image ke database
     try {
-        await Product.update({name: name, image: fileName, url: url}, {
-            where: {
+        await Product.update({name: name, image: fileName, url: url},{
+            where:{
                 id: req.params.id
             }
         });
-        res.status(200).json({msg: "Product Updated successfully"});
+        res.status(200).json({msg: "Product Updated Successfuly"});
     } catch (error) {
         console.log(error.message);
     }
 }
 
-export const deleteProduct = async (req, res) =>{
+export const deleteProduct = async(req, res)=>{
     const product = await Product.findOne({
-        where: {
+        where:{
             id : req.params.id
         }
     });
     if(!product) return res.status(404).json({msg: "No Data Found"});
 
     try {
-        const filePath = `./public/images/${product.image}`;
-        fs.unlinkSync(filePath);
+        const filepath = `./public/images/${product.image}`;
+        fs.unlinkSync(filepath);
         await Product.destroy({
-            where: {
+            where:{
                 id : req.params.id
             }
         });
-        res.status(200).json({msg: "Product Deleted Succesfully"});
+        res.status(200).json({msg: "Product Deleted Successfuly"});
     } catch (error) {
         console.log(error.message);
     }
--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const ProductList = () => {
     const [products, setProducts] = useState([]);
@@ -11,37 +12,56 @@ const ProductList = () => {
     const getProducts = async () => {
         const response = await axios.get("http://localhost:5000/products");
         setProducts(response.data);
-    }
+    };
+
+    const deleteProduct = async (productId) => {
+        try {
+        await axios.delete(`http://localhost:5000/products/${productId}`);
+        getProducts();
+        } catch (error) {
+        console.log(error);
+        }
+    };
 
     return (
         <div className="container mt-5">
-            <div className="columns is-multiline">
-                {products.map((product) => (
-                <div className="column is-one-quarter" key={product.id}>
-                    <div className="card">
-                    <div className="card-image">
-                        <figure className="image is-4by3">
-                        <img src={product.url}
-                        alt="Image"/>
-                        </figure>
+        <Link to="/add" className="button is-success">
+            Add New
+        </Link>
+        <div className="columns is-multiline mt-2">
+            {products.map((product) => (
+            <div className="column is-one-quarter" key={product.id}>
+                <div className="card">
+                <div className="card-image">
+                    <figure className="image is-4by3">
+                    <img src={product.url} alt="Image" />
+                    </figure>
+                </div>
+                <div className="card-content">
+                    <div className="media">
+                    <div className="media-content">
+                        <p className="title is-4">{product.name}</p>
                     </div>
-                    <div className="card-content">
-                        <div className="media">
-                        <div className="media-content">
-                            <p className="title is-4">{product.name}</p>
-                        </div>
-                        </div>
-                    </div>
-                    <footer className='card-footer'>
-                        <a className='card-footer-item'>Edit</a>
-                        <a className='card-footer-item'>Delete</a>
-                    </footer>
                     </div>
                 </div>
-                ))}
-            </div>
-        </div>
-    )
-}
 
-export default ProductList
+                <footer className="card-footer">
+                    <Link to={`edit/${product.id}`} className="card-footer-item">
+                    Edit
+                    </Link>
+                    <a
+                    onClick={() => deleteProduct(product.id)}
+                    className="card-footer-item"
+                    >
+                    Delete
+                    </a>
+                </footer>
+                </div>
+            </div>
+            ))}
+        </div>
+        </div>
+    );
+};
+
+export default ProductList;
